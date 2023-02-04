c9a9c3a5234790d5a92dc9d3f483fb037429f0b0
Tatevik <tshavarshevna@gmail.com>
2023-02-04 21:01:09 +0300

    0303

---
+++ HWC#3/HWC#0303/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+
+int remineder, sum = 0;
+int temp = number;
+while (number > 0)
+
+{
+
+    remineder = number % 10;
+    sum = (sum * 10) + remineder;
+    number = number / 10; 
+
+}
+if (temp == sum)
+
+{
+    Console.WriteLine($"Number {temp} палидром.");
+}
+    else
+
+{
+    Console.WriteLine($"Number {temp} нет");
+}
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
